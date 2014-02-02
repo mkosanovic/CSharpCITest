01a27d6559553ccbbe6a229cd1f0f3607b2e1905
Matija Kosanovic <matija.kosanovic@hotmail.com>
2014-02-02 16:25:39 +0100

    Project is going up

---
+++ HelloApp.Test/HelloTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+
+namespace HelloApp.Test
+{
+    [TestFixture]
+    public class HelloTest
+    {
+        [Test]
+        public void WorldIsSafe()
+        {
+            // arrange
+            bool isTrue;
+
+            // act
+            isTrue = true;
+
+            // assert
+            isTrue.ShouldBe(true);
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+        }
+    }
+}
